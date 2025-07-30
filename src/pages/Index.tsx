366ef30166aaefee3f1e17d004bafb958567814c
Sandalu Karunasena <sandalu.18@cse.mrt.ac.lk>
2025-07-30 19:20:15 +0530

    Format sections

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,6 +4,7 @@ import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Experience from "@/components/Experience";
+import Education from "@/components/Education";
 import Contact from "@/components/Contact";
 import Navigation from "@/components/Navigation";
 
@@ -15,8 +16,9 @@ const Index = () => {
         <Hero />
         <About />
         <Skills />
-        <Projects />
         <Experience />
+        <Education />
+        <Projects />
         <Contact />
       </main>
     </div>
